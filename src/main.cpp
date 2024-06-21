8e5aec68e39c57ea0e3ad68bd19abe2eaefcef17
SodiumZincate <darkthunder248@gmail.com>
2024-06-21 20:20:22 +0545

    Added login text and new font

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <QMdiArea>
 #include <QMdiSubWindow>
 #include <QGraphicsScene>
+#include <QLabel>
 //#include <QTextEdit>
 
 int main(int argc, char *argv[]) {
@@ -19,38 +20,47 @@ int main(int argc, char *argv[]) {
     QWidget window;
     window.setWindowTitle("League Manager");
 
+    QFont placeholder_font("Sans", 24);
+    QFont login_text_font("Sans", 36);
+
     QWidget *rectangular_widget = new QWidget(&window);
     QSize container_size(app_width/2, app_width/16);
-    QRect rect_container(app_width/4, app_height/4, app_width/2, app_width/4); // Expanded the width of rect_container
+    QRect rect_container(app_width/4, app_height/8, app_width/2, app_width/4);
+
+    QLabel *login_text = new QLabel(&window);
+    login_text->setFont(login_text_font);
+    login_text->setText("Register: ");
+    login_text->setAlignment(Qt::AlignCenter);
+    login_text->setMargin(60);
 
     rectangular_widget->setGeometry(rect_container);
 
     QVBoxLayout *login_layout = new QVBoxLayout(rectangular_widget);
     login_layout->setAlignment(Qt::AlignCenter); // Center align the contents
 
-    QFont new_font("Sans", 24);
-
     QLineEdit *username_edit = new QLineEdit(rectangular_widget);
     username_edit->setFixedSize(container_size);
-    username_edit->setFont(new_font);
+    username_edit->setFont(placeholder_font);
     username_edit->setPlaceholderText("Username");
-    username_edit->setAlignment(Qt::AlignCenter); // Center align text within QLineEdit
+    username_edit->setAlignment(Qt::AlignLeft); // Center align text within QLineEdit
     username_edit->setTextMargins(20, 0, 0, 5);
 
     QLineEdit *password_edit = new QLineEdit(rectangular_widget);
     password_edit->setFixedSize(container_size);
-    password_edit->setFont(new_font);
+    password_edit->setFont(placeholder_font);
     password_edit->setPlaceholderText("Password");
     password_edit->setEchoMode(QLineEdit::Password);
-    password_edit->setAlignment(Qt::AlignCenter); // Center align text within QLineEdit
+    password_edit->setAlignment(Qt::AlignLeft); // Center align text within QLineEdit
     password_edit->setTextMargins(20, 0, 0, 5);
 
+    login_layout->addWidget(login_text);
     login_layout->addWidget(username_edit);
     login_layout->addWidget(password_edit);
 
     QVBoxLayout *main_layout = new QVBoxLayout(&window);
-    main_layout->addWidget(rectangular_widget);
 
+    main_layout->addWidget(rectangular_widget);
+    
     window.setLayout(main_layout);
     window.resize(scrn->width/2, scrn->height/2);
 
