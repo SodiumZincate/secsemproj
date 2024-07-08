270bf67153c59a84228665ed6c790cf4e5b7e195
SodiumZincate <darkthunder248@gmail.com>
2024-07-08 18:00:56 +0545

    Added inserting into table

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<sstream>
-#include"sqlite3.h"
+#include<sqlite3.h>
 
 using namespace std;
+
+static int callback(void* data, int argc, char **argv, char** azcolName){
+	int i;
+	for(i=0; i<argc; i++){
+		cout << azcolName[i] << " = " << argv[i] << endl; 
+	}
+	cout << endl;
+	return 0;
+}
 
 int main(int argc, const char* argvp[])
 {
@@ -10,14 +19,14 @@ int main(int argc, const char* argvp[])
 	int exit = 0;
 	char *errMsg;
 
-	stringstream ss;
-	ss << "CREATE TABLE DATA"
+	stringstream ss1;
+	ss1 << "CREATE TABLE DATA"
 		<< "(SN INT PRIMARY KEY NOT NULL,"
 		<< "USERNAME TEXT NOT NULL,"
 		<< "EMAIL TEXT NOT NULL,"
 		<< "PASSWORD TEXT NOT NULL);";
 
-	std::string sqlMsg = ss.str();
+	std::string sqlCreate = ss1.str();
 	// cout << sqlMsg << endl;
 
 	exit = sqlite3_open("test.db", &db);
@@ -29,15 +38,42 @@ int main(int argc, const char* argvp[])
 		cout << "Database opened successfully" << endl;
 	}
 
-	exit = sqlite3_exec(db, sqlMsg.c_str(), NULL, 0, &errMsg);
+	exit = sqlite3_exec(db, sqlCreate.c_str(), NULL, 0, &errMsg);
 	if(exit!=SQLITE_OK){
 		cerr << "Error creating table" << endl;
+		cerr << errMsg << endl;
 		sqlite3_free(errMsg);
 	}
 	else{
 		cout << "Table created successfully" << endl;
 	}
 
+	cout << "Table Contents before inserting data: " << endl;
+
+	string sqlQuery = "SELECT * FROM DATA";
+	sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg);
+
+	stringstream ss2;
+	ss2 << "INSERT INTO DATA VALUES"
+	<< "(1,"
+	<< "'Prasiddha',"
+	<< "'prasiddhapokh@gmail.com',"
+	<< "'12345678')";
+
+	string sqlInsert = ss2.str();
+	exit = sqlite3_exec(db, sqlInsert.c_str(), NULL, 0, &errMsg);
+	if(exit!=SQLITE_OK){
+		cerr << "Error inserting data" << endl;
+		cerr << errMsg << endl;
+		sqlite3_free(errMsg);
+	}
+	else{
+		cout << "Data inserted successfully" << endl;
+	}
+
+	cout << "Table Contents after inserting data: " << endl;
+	sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg);
+	
 	sqlite3_close(db);
 	return 0;
 
