d6dce134bdb072db775a198737da182629e031bb
SodiumZincate <darkthunder248@gmail.com>
2024-07-08 17:26:26 +0545

    Added table creation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<sstream>
 #include"sqlite3.h"
 
 using namespace std;
@@ -7,6 +8,18 @@ int main(int argc, const char* argvp[])
 {
 	sqlite3 *db;
 	int exit = 0;
+	char *errMsg;
+
+	stringstream ss;
+	ss << "CREATE TABLE DATA"
+		<< "(SN INT PRIMARY KEY NOT NULL,"
+		<< "USERNAME TEXT NOT NULL,"
+		<< "EMAIL TEXT NOT NULL,"
+		<< "PASSWORD TEXT NOT NULL);";
+
+	std::string sqlMsg = ss.str();
+	// cout << sqlMsg << endl;
+
 	exit = sqlite3_open("test.db", &db);
 	if(exit){
 		cerr << "Failed to open database: " << sqlite3_errmsg(db) << endl;
@@ -15,6 +28,16 @@ int main(int argc, const char* argvp[])
 	else{
 		cout << "Database opened successfully" << endl;
 	}
+
+	exit = sqlite3_exec(db, sqlMsg.c_str(), NULL, 0, &errMsg);
+	if(exit!=SQLITE_OK){
+		cerr << "Error creating table" << endl;
+		sqlite3_free(errMsg);
+	}
+	else{
+		cout << "Table created successfully" << endl;
+	}
+
 	sqlite3_close(db);
 	return 0;
 
