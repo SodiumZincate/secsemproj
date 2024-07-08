4cc1006142a72fbbbb155dad5dca60569af3a352
SodiumZincate <darkthunder248@gmail.com>
2024-07-08 18:19:07 +0545

    Added removing data

--- main.cpp
+++ main.cpp
@@ -13,6 +13,15 @@ static int callback(void* data, int argc, char **argv, char** azcolName){
 	return 0;
 }
 
+void query(sqlite3 *db){
+	char *errMsg;
+	string sqlQuery = "SELECT * FROM DATA";
+	if(sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg) != SQLITE_OK){
+		cerr << "SQL Error: " << errMsg << endl;
+		sqlite3_free(errMsg);
+	}
+}
+
 int main(int argc, const char* argvp[])
 {
 	sqlite3 *db;
@@ -21,10 +30,12 @@ int main(int argc, const char* argvp[])
 
 	stringstream ss1;
 	ss1 << "CREATE TABLE DATA"
-		<< "(SN INT PRIMARY KEY NOT NULL,"
+		<< "("
+		<< "SN INT PRIMARY KEY NOT NULL,"
 		<< "USERNAME TEXT NOT NULL,"
 		<< "EMAIL TEXT NOT NULL,"
-		<< "PASSWORD TEXT NOT NULL);";
+		<< "PASSWORD TEXT NOT NULL"
+		<< ")";
 
 	std::string sqlCreate = ss1.str();
 	// cout << sqlMsg << endl;
@@ -49,16 +60,16 @@ int main(int argc, const char* argvp[])
 	}
 
 	cout << "Table Contents before inserting data: " << endl;
-
-	string sqlQuery = "SELECT * FROM DATA";
-	sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg);
+	query(db);
 
 	stringstream ss2;
 	ss2 << "INSERT INTO DATA VALUES"
-	<< "(1,"
+	<< "("
+	<< "1,"
 	<< "'Prasiddha',"
 	<< "'prasiddhapokh@gmail.com',"
-	<< "'12345678')";
+	<< "'12345678'"
+	<< ")";
 
 	string sqlInsert = ss2.str();
 	exit = sqlite3_exec(db, sqlInsert.c_str(), NULL, 0, &errMsg);
@@ -72,7 +83,26 @@ int main(int argc, const char* argvp[])
 	}
 
 	cout << "Table Contents after inserting data: " << endl;
-	sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg);
+	query(db);
+
+	stringstream ss3;
+	ss3 << "DELETE FROM DATA WHERE USERNAME"
+	<< "="
+	<< "'Prasiddha'";
+
+	string sqlDelete = ss3.str();
+	exit = sqlite3_exec(db, sqlDelete.c_str(), NULL, 0, &errMsg);
+	if(exit!=SQLITE_OK){
+		cerr << "Error deleting data" << endl;
+		cerr << errMsg << endl;
+		sqlite3_free(errMsg);
+	}
+	else{
+		cout << "Data deleted successfully" << endl;
+	}
+
+	cout << "Table Contents after deleting data: " << endl;
+	query(db);
 	
 	sqlite3_close(db);
 	return 0;
