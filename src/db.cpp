282d62092686657ea724e50cccab8acde70e4584
SodiumZincate <darkthunder248@gmail.com>
2024-07-09 02:39:22 +0545

    Added login ID credentials checking

--- src/db.cpp
+++ src/db.cpp
@@ -2,110 +2,42 @@
 
 using namespace std;
 
-// Callback function is called for each row
-// data = database, argc = no. of columns in a row,
-// argv = array of strings where each string is the value of a column in the row
-// azcolName = array of strings where each string is the name of the column
-static int callback(void* data, int argc, char **argv, char** azcolName){
-	int i;
-	for(i=0; i<argc; i++){
-		cout << azcolName[i] << " = " << argv[i] << endl; 
-	}
-	cout << endl;
-	return 0;
-}
-
-void query(sqlite3 *db){
-	char *errMsg;
-	string sqlQuery = "SELECT * FROM DATA";
-	if(sqlite3_exec(db, sqlQuery.c_str(), callback, 0, &errMsg) != SQLITE_OK){
-		cerr << "SQL Error: " << errMsg << endl;
-		sqlite3_free(errMsg);
-	}
-}
-
-int initDatabase(string username, string email, string password)
-{
-	sqlite3 *db;
-	int exit = 0;
-	char *errMsg;
-
-	stringstream ss1;
-	ss1 << "CREATE TABLE DATA"
-		<< "("
-        << "LID INTEGER PRIMARY KEY AUTOINCREMENT, "
-		<< "USERNAME TEXT NOT NULL,"
-		<< "EMAIL TEXT NOT NULL,"
-		<< "PASSWORD TEXT NOT NULL"
-		<< ")";
-
-	std::string sqlCreate = ss1.str();
-	// cout << sqlMsg << endl;
-
-	exit = sqlite3_open("test.db", &db);
-	if(exit){
-		cerr << "Failed to open database: " << sqlite3_errmsg(db) << endl;
-		return 1;
-	}
-	else{
-		cout << "Database opened successfully" << endl;
-	}
-
-	exit = sqlite3_exec(db, sqlCreate.c_str(), NULL, 0, &errMsg);
-	if(exit!=SQLITE_OK){
-		cerr << "Error creating table" << endl;
-		cerr << errMsg << endl;
-		sqlite3_free(errMsg);
-	}
-	else{
-		cout << "Table created successfully" << endl;
-	}
-
-	cout << "Table Contents before inserting data: " << endl;
-	query(db);
-
-	stringstream ss2;
-	int LID = 1;
-	ss2 << "INSERT INTO DATA(USERNAME,EMAIL,PASSWORD) VALUES"
-	<< "("
-	<< "'" << username << "',"
-	<< "'" << email << "',"
-	<< "'" << password << "'"
-	<< ")";
-
-	string sqlInsert = ss2.str();
-	exit = sqlite3_exec(db, sqlInsert.c_str(), NULL, 0, &errMsg);
-	if(exit!=SQLITE_OK){
-		cerr << "Error inserting data" << endl;
-		cerr << errMsg << endl;
-		sqlite3_free(errMsg);
-	}
-	else{
-		cout << "Data inserted successfully" << endl;
-	}
-
-	cout << "Table Contents after inserting data: " << endl;
-	query(db);
-
-	// stringstream ss3;
-	// ss3 << "DELETE FROM DATA WHERE USERNAME"
-	// << "="
-	// << "'" << username << "'";
-
-	// string sqlDelete = ss3.str();
-	// exit = sqlite3_exec(db, sqlDelete.c_str(), NULL, 0, &errMsg);
-	// if(exit!=SQLITE_OK){
-	// 	cerr << "Error deleting data" << endl;
-	// 	cerr << errMsg << endl;
-	// 	sqlite3_free(errMsg);
-	// }
-	// else{
-	// 	cout << "Data deleted successfully" << endl;
-	// }
-
-	// cout << "Table Contents after deleting data: " << endl;
-	// query(db);
+int updateDatabase(string clientReq, string mode, stringstream &clientRes) {
+	httplib::Client cli("0.0.0.0", 8080);
 	
-	sqlite3_close(db);
+	if (strcmp(mode.c_str(), "insert") == 0){
+		if (auto res = cli.Post("/login/insert?=login.db", clientReq, "text/plain")) {
+			cout << res->status << endl;
+			cout << res->get_header_value("Content-Type") << endl;
+		}
+		else {
+			cout << "error code: " << res.error() << std::endl;
+			return 1;
+		}
+	}
+	else if(strcmp(mode.c_str(), "delete") == 0){
+		if (auto res = cli.Post("/login/delete?=login.db", clientReq, "text/plain")) {
+			cout << res->status << endl;
+			cout << res->get_header_value("Content-Type") << endl;
+		}
+		else {
+			cout << "error code: " << res.error() << std::endl;
+			return 1;
+		}
+	}
+	else if(strcmp(mode.c_str(), "query") == 0){
+		if (auto res = cli.Post("/login/query?=login.db", clientReq, "text/plain")) {
+			cout << res->status << endl;
+			cout << res->get_header_value("Content-Type") << endl;
+			cout << res->body << endl;
+			clientRes.clear();
+			clientRes << res->body;
+		}
+		else {
+			cout << "error code: " << res.error() << std::endl;
+			return 1;
+		}
+	}
+	cli.stop();
 	return 0;
 }
