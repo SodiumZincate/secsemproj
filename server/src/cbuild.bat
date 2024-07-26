88e622ca7fcf4bf4e0a5c0a24c01d26505274b3c
Visual Studio <darkthunder248@gmail.com>
2024-07-26 14:51:26 +0545

    updated gitignore

--- server/src/cbuild.bat
+++ server/src/cbuild.bat
@@ -1,3 +1,4 @@
+g++ database.cpp -o database -lsqlite3 -lws2_32
 .\database.exe
 
 g++ -o main.o -c main.cpp -lsqlite3 -lws2_32
--- server/src/cbuild.sh
+++ server/src/cbuild.sh
@@ -1,3 +1,5 @@
+g++ -o database.o -c database.cpp -lsqlite3
+g++ -o database.o database.cpp -lsqlite3
 ./database
 
 g++ -o main.o -c main.cpp -lsqlite3
