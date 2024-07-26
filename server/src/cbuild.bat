78f0c2d7ffdd7378f22b1d2e6b68a0876f40a92a
Visual Studio <darkthunder248@gmail.com>
2024-07-26 14:48:40 +0545

    automated database initialization

--- server/src/cbuild.bat
+++ server/src/cbuild.bat
@@ -1,3 +1,5 @@
+.\database.exe
+
 g++ -o main.o -c main.cpp -lsqlite3 -lws2_32
 g++ -o main main.o -lsqlite3 -lws2_32
 .\main.exe
--- server/src/cbuild.sh
+++ server/src/cbuild.sh
@@ -1,4 +1,5 @@
+./database
+
 g++ -o main.o -c main.cpp -lsqlite3
-# g++ -o data_man.o -c data_man.cpp -lsqlite3
 g++ -o main main.o -lsqlite3
 ./main
